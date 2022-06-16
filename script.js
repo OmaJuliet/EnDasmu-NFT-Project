d50371dfa017d75094ac39114a250089bfd5d1d4
Juliet <julieto2001@gmail.com>
2022-06-16 23:41:26 +0100

    Worked on the Hero and Artwork Section

---
+++ script.js
@@ -0,0 +1,5 @@
+// Javascript for Hero/Nav section assigned to Juliet
+const overlayMenu = document.querySelector(".nav-overlay");
+const toggleMenu = () => {
+  overlayMenu.classList.toggle("open");
+};
